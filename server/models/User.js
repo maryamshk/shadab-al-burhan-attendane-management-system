825dafac73abf74d0cf0c49950be7b0c78b39d5c
Maryam <maryamjaved.xo@gmail.com>
2023-08-31 14:56:58 +0500

    authentication

--- server/models/User.js
+++ server/models/User.js
@@ -1,19 +1,44 @@
 const mongoose = require('mongoose');
+const { isEmail } = require("validator");
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, "Please enter an email"],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, "Please enter a valid email"], //isEamil will check that email is valid or not
     },
 
     password: {
         type: String,
-        required: true,
-        minLength: 6
+        required: [true, "Please enter a password"],
+        minLength: [6, "Minimum password length is 6 characters"],
     },
 });
+
+// fire a function before doc saved to db
+userSchema.pre("save", async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+// Static method to login user
+userSchema.statics.login = async function (email, password) {
+    const user = await this.findOne({ email }); // this here refers to user. It will first check email exists or not
+    if (user) {
+        // comparing hashed password
+        const auth = await bcrypt.compare(password, user.password); //if email exists so we will compare its hashed password with the one saved in database if it matches return to screen otherwise throw error
+        if (auth) {
+            return user; //Password matched
+        }
+        throw Error("incorrect password");
+    }
+    throw Error("incorrect email");
+};
+
 
 const User = mongoose.model('user', userSchema);
 module.exports = User;
--- server/server.js
+++ server/server.js
@@ -1,26 +1,32 @@
 const express = require('express');
 const authRoutes = require('./routes/authRoutes');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
+
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
-
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
 
-// app.get('/api', (req, res) => {
-//     res.json({ "users": ["user1", "user2", "user3"] })
-// })
+//app.get('/api', (req, res) => {
+//res.json({ "users": ["user1", "user2", "user3"] })
+//})
 
-const dbIRI = 'mongodb+srv://alibaba1990:19901990@cluster0.mcpgbvc.mongodb.net/?retryWrites=true&w=majority';
+//const dbIRI = 'mongodb+srv://alibaba1990:19901990@cluster0.mcpgbvc.mongodb.net/?retryWrites=true&w=majority';
+const dbIRI = 'mongodb+srv://data70:cute12@cluster0.vk5gxdo.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(dbIRI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => app.listen(5000))
     .catch((err) => console.log(err));
 
-// app.listen(5000, () => console.log("listening"))
 
-// app.get('/signup', (req, res) => res.render('signup'))
+
+app.get('*', checkUser);    //its going to be applied to every single get request/route
+app.get('/', (req, res) => res.render('home'));
 app.use(authRoutes);
